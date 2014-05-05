3e75a211f59e3d288001c021e0634ac773350580
Adam Thede <adam@thedetech.com>
2014-05-05 16:06:12 -0500

    New files for menu integration testing :neckbeard:

---
+++ spec/integration/menu_integration_spec.rb
@@ -0,0 +1,68 @@
+require_relative '../spec_helper'
+
+describe "Menu Integration" do
+  let(:menu_text) do
+<<EOS
+What do you want to do?
+1. Add person.
+2. Tell me a story.
+3. Display 5 most recent entries.
+EOS
+  end
+
+  context "the menu displays on startup" do
+    let(:shell_output){ run_doogie_with_input() }
+    it "should print the main menu" do
+      shell_output.should include(menu_text)
+    end
+  end
+
+  context "the user selects 1" do
+    let(:shell_output){ run_doogie_with_input("1") }
+    it "should print the next menu" do
+      shell_output.should include("What is your name?")
+    end
+  end
+
+  context "the user selects 2" do
+    let(:shell_output){ run_doogie_with_input("2") }
+    it "should print the next menu" do
+      shell_output.should include("...")
+    end
+  end
+
+  context "the user selects 3" do
+    let(:shell_output){ run_doogie_with_input("3") }
+    it "should print the next menu" do
+      shell_output.should include("...")
+    end
+  end
+
+  context "prompt for correct input if user inputs incorrect selection" do
+    let(:shell_output){ run_doogie_with_input("4") }
+    it "should print the menu again" do
+      shell_output.should include_in_order(menu_text, "4", menu_text)
+    end
+    it "should include an appropriate error message" do
+      shell_output.should include("'4' is not a valid selection")
+    end
+  end
+
+  context "if the user types in no input" do
+    let(:shell_output){ run_doogie_with_input("") }
+    it "should print the menu again" do
+      shell_output.should include_in_order(menu_text, "", menu_text)
+    end
+    it "should include an appropriate error message" do
+      shell_output.should include("'' is not a valid selection.")
+    end
+  end
+
+  context "if the user types in incorrect input multiple times, it should allow correct input" do
+    let(:shell_output){ run_doogie_with_input("4", "", "1") }
+    it "should include the appropriate menu" do
+      shell_output.should include("What is your name?")
+    end
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'rspec/expectations'
+
+$LOAD_PATH << "lib"
+
+def run_doogie_with_input(*inputs)
+  shell_output = ""
+  IO.popen('./doogie', 'r+') do |pipe|
+    inputs.each do |input|
+      pipe.puts input
+    end
+    pipe.close_write
+    shell_output << pipe.read
+  end
+  shell_output
+end
+
+RSpec::Matchers.define :include_in_order do |*expected|
+  match do |actual|
+    input = actual.delete("\n")
+    regexp_string = expected.join(".*").gsub("?","\\?").gsub("\n",".*")
+    result = /#{regexp_string}/.match(input)
+    result.should be
+  end
+end
