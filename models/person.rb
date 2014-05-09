5a40933cdc5b588228028160547cd6317f0f793c
Adam Thede <adam@thedetech.com>
2014-05-09 15:59:48 -0500

    And here are the necessary files for the Person class :sweat:

---
+++ models/person.rb
@@ -0,0 +1,67 @@
+class Person
+  attr_reader :errors
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    if self.valid?
+      statement = "INSERT INTO people (name) values (?);"
+      Environment.database_connection.execute(statement, name)
+      true
+    else
+      false
+    end
+  end
+
+  def valid?
+    @errors = []
+    if !name.match /[a-zA-Z]/
+      @errors << "You didn't include any letters!  Surely your name must have letters in it."
+    end
+    if Person.find_by_name(self.name)
+      @errors << "Sorry, '#{self.name}' already exists."
+    end
+    @errors.empty?
+  end
+
+  def self.find_by_name(name)
+    statement = "SELECT * FROM people WHERE name = ?;"
+    result = execute_and_instantiate(statement, name)
+    unless result.empty?
+      result[0]
+    end
+  end
+
+  def self.all
+    statement = "SELECT * FROM people;"
+    execute_and_instantiate(statement)
+  end
+
+  def self.count
+    statement = "SELECT COUNT(*) FROM people;"
+    result = Environment.database_connection.execute(statement)
+    result[0][0]
+  end
+
+  def self.last
+    statement = "SELECT * FROM people ORDER BY id DESC LIMIT(1);"
+    result = execute_and_instantiate(statement)
+    unless result.empty?
+      result[0]
+    end
+  end
+
+  private
+
+  def self.execute_and_instantiate(statement, bind_vars = [])
+    rows = Environment.database_connection.execute(statement, bind_vars)
+    results = []
+    rows.each do |row|
+      results << Person.new(row["name"])
+    end
+    results
+  end
+end
---
+++ spec/integration/person_integration_spec.rb
@@ -0,0 +1,45 @@
+require_relative '../spec_helper'
+
+describe "Adding a person" do
+  before do
+    person = Person.new("Eliza")
+    person.save
+  end
+
+  context "adding a unique person" do
+    let!(:output){ run_doogie_with_input("1", "John") }
+    it "should print a successful confirmation message" do
+      output.should include("Hi, John! Welcome to Doogie Howser, CLI")
+      Person.count.should == 2
+    end
+    it "should insert a new person in the db" do
+      Person.count.should == 2
+    end
+    it "should save the name we entered" do
+      Person.last.name.should == "John"
+    end
+  end
+
+  context "adding a duplicate person" do
+    let(:output){ run_doogie_with_input("1", "Eliza") }
+    it "should print an error message" do
+      output.should include("Sorry, 'Eliza' already exists.")
+    end
+    it "should ask them to try again" do
+      menu_text = "What is your name?"
+      output.should include_in_order(menu_text, "already exists", menu_text)
+    end
+    it "shouldn't save the duplicate" do
+      Person.count.should == 1
+    end
+    context "and trying again" do
+      let!(:output){ run_doogie_with_input("1", "Eliza", "John") }
+      it "should save a unique item" do
+        Person.last.name.should == "John"
+      end
+      it "should print a success message at the end" do
+        output.should include("Hi, John! Welcome to Doogie Howser, CLI")
+      end
+    end
+  end
+end
